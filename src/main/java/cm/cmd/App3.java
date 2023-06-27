558860782d454be85698559a1e168a2a0f070f2b
elysetonke <elyse.tonke@gmail.com>
2023-06-27 15:26:03 -0400

    Ligne trois App3.java

--- src/main/java/cm/cmd/App3.java
+++ src/main/java/cm/cmd/App3.java
@@ -6,41 +6,36 @@ import cm.cmd.metier.BankAccountServiceImpl;
 import cm.cmd.model.BankAccount;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-
 public class App3 {
 
     public static void main(String[] args) throws JsonProcessingException {
-        BankAccountService bankAccountService=new BankAccountServiceImpl();
+        BankAccountService bankAccountService = new BankAccountServiceImpl();
         bankAccountService.addRandomData(20);
-        for(BankAccount bankAccount:bankAccountService.getAllAccounts()){
-           System.out.println(bankAccount.toString());
+        for (BankAccount bankAccount : bankAccountService.getAllAccounts()) {
+            System.out.println(bankAccount.toString());
 
         }
 
-
-
-        for(BankAccount acc:bankAccountService.getAllAccounts()){
+        for (BankAccount acc : bankAccountService.getAllAccounts()) {
             System.out.println(DataTransformationUtils.toJson(acc));
 
         }
 
         System.out.println("================\n=========== list des Saving Accounts");
-        for(BankAccount acc: bankAccountService.getSavingAccounts()){
+        for (BankAccount acc : bankAccountService.getSavingAccounts()) {
             System.out.println(DataTransformationUtils.toJson(acc));
         }
-
-
-
 
         System.out.println("================\n=========== list des CURRENT ACCOUNT");
-        for(BankAccount acc: bankAccountService.getCurrentAccounts()){
+        for (BankAccount acc : bankAccountService.getCurrentAccounts()) {
             System.out.println(DataTransformationUtils.toJson(acc));
         }
-
 
         System.out.println("================\n=========== Total des balances");
 
-        System.out.println("Total Balance: "+bankAccountService.getTotalBalance() );
+        System.out.println("Total Balance: " + bankAccountService.getTotalBalance());
+
+        System.out.println("Tout est bien qui fini bien ");
     }
 
 }
